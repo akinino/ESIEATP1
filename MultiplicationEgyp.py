6e5011151a365086107325daeff77ae738edd524
akinino <74606334+akinino@users.noreply.github.com>
2022-03-01 11:07:54 +0100

    Add files via upload

---
+++ MultiplicationEgyp.py
@@ -0,0 +1,15 @@
+def Egypt(x,y):
+	resultat = 0
+
+	while(y > 0):
+		if(y % 2 == 0):
+			x *= 2
+			y /= 2
+		else:
+			resultat += x
+			y -= 1
+	print("Le Resultat est = ",resultat)
+
+x = int(input("Donnez la valeur de x = "))
+y = int(input("Donnez la valeur de y = "))
+Egypt(x,y)
